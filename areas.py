272a4d37fdbd3758c456046befaddcb0e076f3c6
normagomezb <160788864+normagomezb@users.noreply.github.com>
2024-09-16 18:52:22 -0600

    Actualizacion del archivo areas

--- areas.py
+++ areas.py
@@ -5,6 +5,11 @@ AreaCirculo = math.pi * r ** 2
 
 l = int(input("Dame la longitud de un lado del cuadrado: "))
 AreaCuadrado = l * l
-        
+
+B = int(input("Dame la base dek triangulo: "))
+H = int(input("Dame la altura del triangulo: "))
+areaTriangulo = ((B*H)/(2))
+
 print(f"El área del círculo es: {AreaCirculo}")
 print(f"El área del cuadrado es: {AreaCuadrado}")
+print(f"El área del triangulo Equilatero es: {areaTriangulo}")
